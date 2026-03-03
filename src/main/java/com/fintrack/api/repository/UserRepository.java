c180e9c17ebab8fdb61e3ac7c2c0b69bc82df3c5
helozer <maxxximderkachev@gmail.com>
2026-03-03 14:47:19 +0300

    initial

--- src/main/java/com/fintrack/api/repository/UserRepository.java
+++ src/main/java/com/fintrack/api/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.fintrack.api.entity.User;
 import java.util.List;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query; // <--- Добавлен импорт
 
 public interface UserRepository extends JpaRepository<User, Long> {
     /**
@@ -11,5 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
      * to avoid the N+1 select problem when accessing the accounts collection.
      */
     @EntityGraph(attributePaths = "accounts")
+    @Query("SELECT u FROM User u") // <--- Добавлена эта строка
     List<User> findAllWithAccounts();
 }
