4356b995f1394b3794bea0fb7ad1487d603a560d
helozer <maxxximderkachev@gmail.com>
2026-03-23 13:42:54 +0300

    feat: bug fixes

--- src/main/java/com/fintrack/api/config/OpenApiConfig.java
+++ src/main/java/com/fintrack/api/config/OpenApiConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class OpenApiConfig {
     @Bean
--- src/main/java/com/fintrack/api/repository/TransactionRepository.java
+++ src/main/java/com/fintrack/api/repository/TransactionRepository.java
@@ -19,7 +19,8 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
     @EntityGraph(attributePaths = {"account", "category", "tags"})
     List<Transaction> findAllWithDetails();
 
-    @Query("SELECT DISTINCT t FROM Transaction t JOIN FETCH t.account a JOIN FETCH a.user u JOIN FETCH t.category WHERE u.name = :userName")
+    @Query("SELECT DISTINCT t FROM Transaction t"
+           + " JOIN FETCH t.account a JOIN FETCH a.user u JOIN FETCH t.category WHERE u.name = :userName")
     Page<Transaction> findByUserName(@Param("userName") String userName, Pageable pageable);
 
     @Query(value = "SELECT t.* FROM transactions t "
